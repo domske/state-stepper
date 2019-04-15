5b05bc20012375f64cf68999fafa3d236e1189d1
Dominik Geng <domske@mailbox.org>
2019-04-15 05:11:24 +0200

    css host removed.

--- src/components/state-stepper/state-stepper.component.css.ts
+++ src/components/state-stepper/state-stepper.component.css.ts
@@ -1,23 +1,23 @@
 export default `
-:host .stepper {
+.stepper {
   text-align: center;
   padding: 10px 0px;
   overflow: auto;
   white-space: nowrap;
   user-select: none;
 }
-:host .stepper .step {
+.stepper .step {
   display: inline-block;
   position: relative;
   width: 100px;
 }
-:host .stepper .step:last-of-type {
+.stepper .step:last-of-type {
   margin-right: 0;
 }
-:host .stepper .step:last-of-type::before {
+.stepper .step:last-of-type::before {
   display: none;
 }
-:host .stepper .step .icon {
+.stepper .step .icon {
   width: 30px;
   height: 30px;
   background-color: #989aa2;
@@ -28,16 +28,16 @@ export default `
   transition: all 300ms ease-out;
   will-change: transform;
 }
-:host .stepper .step .text {
+.stepper .step .text {
   font-size: 0.8em;
   opacity: 0.5;
   transition: all 300ms ease-out;
   will-change: opacity;
 }
-:host .stepper .step .text.active {
+.stepper .step .text.active {
   opacity: 1;
 }
-:host .stepper .step::before {
+.stepper .step::before {
   position: absolute;
   content: "";
   background-color: #989aa2;
@@ -48,41 +48,41 @@ export default `
   transition: all 300ms ease-out;
   will-change: background-color;
 }
-:host .stepper .step.active .icon-wrapper {
+.stepper .step.active .icon-wrapper {
   animation: active 1s 300ms infinite ease-in-out;
 }
-:host .stepper .step.active .icon {
+.stepper .step.active .icon {
   transform: scale(1);
 }
-:host .stepper .step.current .icon {
+.stepper .step.current .icon {
   background-color: #3880ff;
   color: #ffffff;
 }
-:host .stepper .step.danger .icon-wrapper {
+.stepper .step.danger .icon-wrapper {
   animation: danger 600ms 300ms ease-in-out;
 }
-:host .stepper .step.danger .icon {
+.stepper .step.danger .icon {
   background-color: #f04141;
   color: #ffffff;
 }
-:host .stepper .step.warning .icon-wrapper {
+.stepper .step.warning .icon-wrapper {
   animation: warn 1s 300ms ease-in-out;
 }
-:host .stepper .step.warning .icon {
+.stepper .step.warning .icon {
   background-color: #ffce00;
   color: #ffffff;
 }
-:host .stepper .step.warning::before {
+.stepper .step.warning::before {
   background-color: #ffce00;
 }
-:host .stepper .step.success .icon-wrapper {
+.stepper .step.success .icon-wrapper {
   animation: success 1s 300ms;
 }
-:host .stepper .step.success .icon {
+.stepper .step.success .icon {
   background-color: #10dc60;
   color: #ffffff;
 }
-:host .stepper .step.success::before {
+.stepper .step.success::before {
   background-color: #10dc60;
 }
 @keyframes active {
