6ed5c57974e5b8c69a79a63991578cf1a3899bea
Dominik Geng <domske@mailbox.org>
2019-09-26 20:57:38 +0200

    New demo.

--- docs/build/state-stepper.esm.js
+++
@@ -1,6 +0,0 @@
-import { p as patchBrowser, g as globals, b as bootstrapLazy } from './core-5e328182.js';
-
-patchBrowser().then(options => {
-  globals();
-  return bootstrapLazy([["state-stepper",[[1,"state-stepper",{"size":[1],"stepNext":[64],"resetSteps":[64],"stepBack":[64]}]]],["state-stepper-step",[[1,"state-stepper-step",{"state":[1]}]]]], options);
-});
